210c79aa9e12256ffdc6d4ef70898186d1099620
Prayag Patel <prayagpatel283@gmail.com>
2026-05-14 15:43:40 -0400

    Renamed the file holding the auto differentiation system to autodiff, implemented a closure function called _backward() that implements the accumulation of gradients depending on the operator, implemented a topological sort method, and implemented the reverse mode automatic differentiation

--- Readme.md
+++ Readme.md
@@ -5,3 +5,6 @@ networks like MINST classifiers using custom made tensor, autograd, basic neural
 network layers, SGD and Adam, dataloaders, checkpointing, training loop, vectorized operations, and a MINST demo using this infastructure.
 
 I am going to be using Python 3.14.4, Numpy for memory storage, and Matplotlib for loss curves, debugging, and other visualizations.
+
+The goal of this project is to create a neural network that can classify shapes as an example using this machine learning framework only. This would show that the framework
+works as it is intended and will give the entire project some direction to follow.
---
+++ src/autodiff/engine.py
@@ -0,0 +1,150 @@
+# Implementation Date: May 10, 2026
+# Author: PrayagPatel1 
+# Topic: Automatic Differentiation - Reverse Mode
+
+class Primal:
+    """
+    Represents a node within a computational graph for representing Mathematical
+    expressions. This will carry out reverse mode automatic differentiation.
+
+    data: float - Represents the data that a node possess after an operation has
+                  been applied to it. 
+    grad: float - Represents the gradient accumulation of the current node. 
+    _op: str - Represents the operation used to modify the <data>.
+    _parents: set() - Represents the current Primal parents. 
+    """
+    data: float
+    grad: float
+
+    def __init__(self, data: float, label: str, _op="", _parents=None):
+        self.data = data
+        self.grad = 0.0
+        self.label = label
+
+        self._op = _op
+        if _parents is not None:
+            self._parents = _parents
+        self._parents = set()
+
+        self._backward = lambda: None
+
+    def topo_sort(self) -> list[Primal]:
+        """
+        Returns a list of all primals within the compute graph ordered 
+        topologically. 
+        """
+        visited = set()
+        topo_elems = []
+
+        def build(primal):
+            if primal not in visited:
+                visited.add(primal)
+            
+            for child in primal._parents:
+                build(child)
+            
+            topo_elems.append(primal)
+        
+        build(self)
+        return topo_elems
+
+    def backward(self):
+        """
+        Performs reverse mode auto differentiation
+        """
+        topo_elems = self.topo_sort
+        self.grad = 1.0 
+
+        for primal in reversed(topo_elems):
+            primal._backward
+    
+    def __add__(self, other):
+        out = Primal(self.data + other.data, "",  "+", {self, other})
+        
+        def _backward():
+            self.grad += out.grad * 1.0 
+            other.grad += out.grad * 1.0
+        
+        out._backward = _backward
+        return out
+    
+    def __sub__(self, other):
+        out = Primal(self.data - other.data, "",  "-", {self, other})
+
+        def _backward():
+            self.grad += out.grad * 1.0 
+            other.grad += out.grad * -1.0
+
+        out._backward = _backward
+        return out
+    
+    def __mul__(self, other):
+        out = Primal(self.data * other.data, "", "*", {self, other})
+
+        def _backward():
+            self.grad += out.grad * other.data
+            other.grad += out.grad * self.data
+
+        out._backward = self._backward
+        return out
+    
+    def __truediv__(self, other):
+        out = Primal(self.data / other.data, "", "/", {self, other})
+
+        def _backward():
+            self.grad += out.grad * (1 / other.data)
+            self.other += out.grad * ((-self.data) / (other.data ** 2))
+        
+        out._backward = self._backward
+        return out
+    
+    def __repr__(self):
+        return f"Value(data={self.data}, \n op={self._op}, \n parents={self._parents}, \n label={self.label})"
+
+# class CompGraph:
+#     """
+#     Represents the computational graph data structure which is a directed acylic
+#     graph (DAG). 
+
+#     primals - A list of Primal nodes that have been defined prior.
+#     nodes - A collection of nodes that is the graph.
+#     """
+
+#     primals: list[Primal]
+#     nodes: dict[str: Primal]
+
+#     def __init__(self, primals: list[Primal]):
+#         self.primals = primals
+#         self.nodes = {};
+
+#         for primal in self.primals:
+
+#             if (not primal.getParents()):
+#                 self.nodes[primal.label] = []
+            
+#             # The case where the primal has parents
+#             if (primal.label in self.nodes):
+#                 for parent in primal.getParents():
+#                     self.nodes[primal.label].append(parent)
+#             else:
+#                 self.nodes[primal.label] = []
+#                 for parent in primal.getParents():
+#                     self.nodes[primal.label].append(parent)
+    
+#     def __repr__(self):
+#         strings = []
+#         for label, primals in self.nodes.items():
+#             connections = []
+#             for primal in primals:
+#                 connections.append(primal)
+            
+#             strings.append(f"{label} -> {connections}")
+        
+#         return "CompGraph: \n" + "\n".join(strings)
+
+
+
+
+            
+
+
