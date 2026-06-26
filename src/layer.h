972f2d7c64e3100e81233168da80e4f01f451e6b
Prayag Patel <prayagpatel283@gmail.com>
2026-06-26 13:53:07 -0400

    Moved all layer implementation code to a different header and c file

---
+++ src/layer.h
@@ -0,0 +1,36 @@
+#ifndef LAYER_H
+#define LAYER_H
+
+// POSIX header files
+#include <stdlib.h>
+
+// Custom-made header files
+#include "vector.h"
+#include "matrix.h"
+
+typedef void (*activation_func)(vector *);
+
+typedef struct layer
+{
+    size_t input_size;
+    size_t output_size; // same as the number of neurons
+
+    matrix weights; // shape: output_size * input_size
+    vector biases;  // shape: output_size
+
+    vector prev_z;      // cached for backpropagation
+    vector prev_input;  // cached for backpropagation
+    vector prev_output; // cached for backpropagation
+
+    activation_func func;
+} layer;
+
+void init_weight_basic(matrix *weights); // TODO: later create a header with just differnet weight initializer
+
+layer layer_init(size_t input_size, size_t output_size, activation_func func);
+
+void layer_free(layer *curr_layer);
+
+void forward_prop_layer(vector inputs, layer *layer);
+
+#endif
