506a8a6f138f23c9713bb783d29c78c9b4a42bbc
Prayag Patel <prayagpatel283@gmail.com>
2026-06-27 14:44:56 -0400

    created a master header file to manage the rest of the header files

---
+++ include/neural_net_lib.h
@@ -0,0 +1,11 @@
+#ifndef NEURAL_NET_LIB_H
+#define NEURAL_NET_LIB_H
+
+#include "../vector.h"
+#include "../matrix.h"
+#include "../activation.h"
+#include "../layer.h"
+#include "../network.h"
+#include "../loss.h"
+
+#endif
